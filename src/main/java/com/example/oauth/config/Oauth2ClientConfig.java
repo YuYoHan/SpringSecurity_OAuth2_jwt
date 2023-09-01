27048ae8c0db9d80917a8ef4906e9360cd05d628
YuYoHan <zxzz8014@naver.com>
2023-09-01 20:07:34 +0900

    [Study] OAuth2

--- src/main/java/com/example/oauth/config/Oauth2ClientConfig.java
+++ src/main/java/com/example/oauth/config/Oauth2ClientConfig.java
@@ -1,13 +1,21 @@
 package com.example.oauth.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class Oauth2ClientConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -16,8 +24,25 @@ public class Oauth2ClientConfig {
                 .authenticated();
 
         http
+                .logout()
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                                .invalidateHttpSession(true)
+                                        .clearAuthentication(true)
+                                                .deleteCookies("JSESSIONID");
+
+        http
                 .oauth2Login(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/login");
+
+        return successHandler;
+
     }
 }
--- src/main/java/com/example/oauth/controller/IndexController.java
+++ src/main/java/com/example/oauth/controller/IndexController.java
@@ -80,4 +80,5 @@ public class IndexController {
 
         return oAuth2User;
     }
+
 }
