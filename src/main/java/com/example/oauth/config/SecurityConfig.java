018b5e6ba7727e21996fa2fe3ea0391d844ee5f3
YuYoHan <zxzz8014@naver.com>
2024-02-08 20:46:16 +0900

    [Study] OAuth2

--- src/main/java/com/example/oauth/config/SecurityConfig.java
+++ src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.example.oauth.config;
 
+import com.example.oauth.config.OAuth2.CustomAuthenticationEntryPoint;
 import com.example.oauth.config.OAuth2.CustomSecurityConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest()
@@ -18,6 +23,16 @@ public class SecurityConfig {
 
         http
                 .formLogin();
+
+        http
+                .httpBasic();
+
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
 
         http
                 .apply(new CustomSecurityConfigurer().setFlag(false));
