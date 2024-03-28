36925f8905421503cc0b29b425df99e2fdb4d54b
YuYoHan <zxzz8014@naver.com>
2024-03-28 22:22:33 +0900

    [Study] OAuth2

--- src/main/java/com/example/oauth/controller/Index2Controller.java
+++ src/main/java/com/example/oauth/controller/Index2Controller.java
@@ -1,17 +1,27 @@
 package com.example.oauth.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class Index2Controller {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping("/v2")
     public String index(Model model,
@@ -32,5 +42,24 @@ public class Index2Controller {
             }
         }
         return "";
+    }
+
+    @GetMapping("/user")
+    public OAuth2User user(String accessToken) {
+        ClientRegistration clientRegistration =
+                clientRegistrationRepository.findByRegistrationId("keycloak");
+        OAuth2AccessToken oAuth2AccessToken =
+                new OAuth2AccessToken(
+                        OAuth2AccessToken.TokenType.BEARER,
+                        accessToken,
+                        Instant.now(),
+                        Instant.MAX);
+
+        OAuth2UserRequest oAuth2UserRequest =
+                new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
+
+        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+        return oAuth2User;
     }
 }
