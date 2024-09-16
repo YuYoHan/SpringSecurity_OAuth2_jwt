bff51f323a58ef2655ea6977f332a0c3eb165e4b
YuYoHan <zxzz8014@naver.com>
2024-09-16 22:31:08 +0900

    [Study] security

--- src/main/java/org/example/security_jwt_oauth2/config/security/SecurityConfig.java
+++ src/main/java/org/example/security_jwt_oauth2/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -48,7 +49,7 @@ public class SecurityConfig {
 
         http
                 .sessionManagement(auth -> auth
-                        .sessionFixation().changeSessionId());
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
         return http.build();
